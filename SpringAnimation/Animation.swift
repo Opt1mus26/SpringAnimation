8e38271b67fca56a5cdea76c6a237c7e7c7834e7
Pavel <92750573+Opt1mus26@users.noreply.github.com>
2021-11-24 19:51:42 +0300

    complete Animation

--- SpringAnimation/Animation.swift
+++ SpringAnimation/Animation.swift
@@ -5,7 +5,28 @@
 //  Created by Pavel Tsyganov on 24.11.2021.
 //
 
-
 struct Animation {
+    let preset: String
+    let curve: String
+    let force: Float
+    let duration: Float
+    let delay: Float
     
+    var descriptionAnimation: String {
+        """
+            Preset: \(preset)
+            curve: \(curve)
+            force: \(force)
+            duration: \(duration)
+            delay: \(delay)
+        """
+    }
+    
+    static func getRandomAnimation() -> Animation {
+        Animation(preset: DataManager.shared.animations.randomElement()?.rawValue ?? "fadeIn",
+                  curve: DataManager.shared.curves.randomElement()?.rawValue ?? "linear",
+                  force: Float.random(in: 0.3...1.6),
+                  duration: Float.random(in: 0.7...2.5),
+                  delay: Float.random(in: 0.2...0.4))
+    }
 }
